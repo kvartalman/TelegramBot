3cf5174ecce3bb5126debc22104b57f8807cac87
kvartalman <ilyakov@yandex.ru>
2023-05-05 20:45:26 +0400

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -11,7 +11,12 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda x: True)        # Обрабатываем любое сообщение, поэтому функция всегда True
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    if message.text == 'Чел ты':
+        bot.reply_to(message, 'Скорее чел ты')
+    elif message.text == 'Офф с позором':
+        bot.reply_to(message, 'Офф с позором? Это офф с супер честью!')
+    else:
+        bot.reply_to(message, 'Я ещё пока ничего не умею, но скоро научусь')
 
 
 bot.infinity_polling()
